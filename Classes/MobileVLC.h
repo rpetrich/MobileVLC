75b20aa9a609244c6134723bf35a223c9e235716
Romain Goyet <romain.goyet@likid.org>
2010-07-13 14:24:03 +0200

    Adding a project-wide header

---
+++ Classes/MobileVLC.h
@@ -0,0 +1,17 @@
+//
+//  MobileVLC.h
+//  MobileVLC
+//
+//  Created by Romain Goyet on 13/07/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#ifdef DEBUG
+#define MVLCLog(format, ...) NSLog(format, ## __VA_ARGS__)
+#define MVLCAssert(format, ...) NSAssert(format, ## __VA_ARGS__)
+#define MVLCIgnoreUnusedParameter(parameter) (void)parameter;
+#else
+#define MVLCLog(format, ...)
+#define MVLCAssert(format, ...)
+#define MVLCIgnoreUnusedParameter(parameter)
+#endif
