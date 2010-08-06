148b9c53767c7a31a099e741c4003db9cae2cc7f
Romain Goyet <romain.goyet@likid.org>
2010-08-06 12:38:00 +0200

    Adding a nifty frame around the thumbnails, and a beautiful gloss effect

--- Classes/MVLCMovieGridViewCell.m
+++ Classes/MVLCMovieGridViewCell.m
@@ -18,6 +18,7 @@
 @interface MVLCMovieGridViewCell (Private)
 + (MVLCMovieGridViewCell *)_cellFromNib;
 - (void)_refreshFromFile;
+- (UIImage *)_framedImageFromImage:(UIImage *)sourceImage;
 @end
 
 @implementation MVLCMovieGridViewCell
@@ -107,11 +108,34 @@
         NSLog(@"%@", url);
         [self.posterImageView setImageWithURL:url];
     } else if (file.computedThumbnail) {
-        [self.posterImageView setImage:[UIImage imageWithData:file.computedThumbnail]];
+        [self.posterImageView setImage:[self _framedImageFromImage:[UIImage imageWithData:file.computedThumbnail]]];
     } else {
         [self.activityIndicator startAnimating];
         [self.posterImageView setImage:nil];
     }
 }
+
+- (UIImage *)_framedImageFromImage:(UIImage *)sourceImage {
+
+	UIImage * maskImage = [UIImage imageNamed:@"MVLCMovieGridViewCellImageMask.png"];	
+	CGContextRef context = NULL;
+
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	context = CGBitmapContextCreate (NULL, maskImage.size.width, maskImage.size.height, 8, 0, colorSpace, kCGImageAlphaPremultipliedLast);	
+	CGColorSpaceRelease(colorSpace);
+
+	CGImageRef maskImageRef = [maskImage CGImage];
+	CGContextClipToMask(context, CGRectMake(0, 0, maskImage.size.width, maskImage.size.height), maskImageRef);
+	CGContextDrawImage(context, CGRectMake(0.0, 0.0, maskImage.size.width, maskImage.size.height), sourceImage.CGImage);
+
+	CGImageRef framedImageRef = CGBitmapContextCreateImage(context);
+	CGContextRelease(context);
+
+	UIImage * framedImage = [UIImage imageWithCGImage:framedImageRef];
+	CGImageRelease(framedImageRef);
+	
+	return framedImage;
+}
+
 @end
 
