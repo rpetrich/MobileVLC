1ac679510488ace401e8a887043e71005cedd5ff
Romain Goyet <romain.goyet@likid.org>
2010-08-11 16:51:50 +0200

    Adding a customized navigation bar

---
+++ Classes/MVLCNavigationBar.h
@@ -0,0 +1,13 @@
+//
+//  MVLCNavigationBar.h
+//  MobileVLC
+//
+//  Created by Romain Goyet on 11/08/10.
+//  Copyright 2010 Applidium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVLCNavigationBar : UINavigationBar {
+}
+@end
---
+++ Classes/MVLCNavigationBar.m
@@ -0,0 +1,28 @@
+//
+//  MVLCNavigationBar.m
+//  MobileVLC
+//
+//  Created by Romain Goyet on 11/08/10.
+//  Copyright 2010 Applidium. All rights reserved.
+//
+
+#import "MVLCNavigationBar.h"
+
+@implementation MVLCNavigationBar
+- (id)initWithCoder:(NSCoder *)aCoder {
+	self = [super initWithCoder:aCoder];
+	if (self != nil) {
+		[self setTintColor:[UIColor colorWithRed:236.0/255.0 green:106.0/255.0 blue:28.0/255.0 alpha:0.2]];
+	}
+	return self;
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+	UIImage *image = [UIImage imageNamed:@"MVLCNavigationBarBackground.png"];
+//	CGContextClip(ctx);
+//	CGContextTranslateCTM(ctx, 0, image.size.height);
+	CGContextScaleCTM(ctx, 1.0, -1.0); // Otherwise the image is drawn upside-down
+//	CGContextDrawImage(ctx, , image.CGImage);
+	CGContextDrawTiledImage(ctx, CGRectMake(0, 0, image.size.width, image.size.height), image.CGImage);
+}
+@end
