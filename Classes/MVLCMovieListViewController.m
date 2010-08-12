78aa7c9b67a10bd043fb5882284f5b23898153d4
Romain Goyet <romain.goyet@likid.org>
2010-08-12 16:31:07 +0200

    Animating the movie selection (first try)

--- Classes/MVLCMovieListViewController.m
+++ Classes/MVLCMovieListViewController.m
@@ -15,6 +15,7 @@
 #import "UIImageView+WebCache.h"
 
 #define MVLC_INSET_BACKGROUND_HEIGHT 600.0f
+#define MVLC_MOVIE_LIST_ANIMATION_DURATION 0.25f
 
 @interface MVLCMovieListViewController (Private)
 - (void)_setBackgroundForOrientation:(UIInterfaceOrientation)orientation;
@@ -124,11 +125,62 @@
 #pragma mark -
 #pragma mark AQGridViewDelegate
 - (void)gridView:(AQGridView *)gridView didSelectItemAtIndex:(NSUInteger)index {
-	MVLCMovieViewController * movieViewController = [[MVLCMovieViewController alloc] init];
-    MLFile *file = [_allMedia objectAtIndex:index];
-	movieViewController.file = file;
-	[self.navigationController pushViewController:movieViewController animated:YES];
-	[movieViewController release];
+	MVLCMovieGridViewCell * cell = [gridView cellForItemAtIndex:index];
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationBeginsFromCurrentState:YES];
+	[UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+	[UIView setAnimationDuration:MVLC_MOVIE_LIST_ANIMATION_DURATION];
+#define MVLC_MOVIE_LIST_ANIMATE_CELL 0
+#if MVLC_MOVIE_LIST_ANIMATE_CELL
+	cell.overlayImageView.alpha = 0.0f;
+	cell.titleLabel.alpha = 0.0f;
+	cell.subtitleLabel.alpha = 0.0f;
+	self.view.alpha = 0.01f;
+	CGAffineTransform transform = CGAffineTransformIdentity;
+	transform = CGAffineTransformTranslate(transform, gridView.bounds.size.width/2.0f - (cell.center.x-gridView.contentOffset.x), gridView.bounds.size.height/2.0f - (cell.center.y - gridView.contentOffset.y));
+	transform = CGAffineTransformScale(transform, 4.0f, 4.0f);
+	transform = CGAffineTransformTranslate(transform, cell.bounds.size.width/2.0f - cell.posterImageView.center.x, cell.bounds.size.height/2.0f - cell.posterImageView.center.y);
+	cell.transform = transform;
+#else
+	CGAffineTransform transform = CGAffineTransformIdentity;
+
+	transform = CGAffineTransformScale(transform, 4.0f, 4.0f);
+	transform = CGAffineTransformTranslate(transform, cell.bounds.size.width/2.0f - cell.posterImageView.center.x, cell.bounds.size.height/2.0f - cell.posterImageView.center.y);
+	transform = CGAffineTransformTranslate(transform, self.view.bounds.size.width/2.0f - (cell.center.x-gridView.contentOffset.x), self.view.bounds.size.height/2.0f - (cell.center.y - gridView.contentOffset.y + 44.0f));
+
+	cell.contentView.backgroundColor = [UIColor blackColor];
+	cell.overlayImageView.alpha = 0.0f;
+	cell.posterImageView.alpha = 0.0f;
+	self.view.transform = transform;
+#endif
+	[UIView commitAnimations];
+//	MVLCMovieViewController * movieViewController = [[MVLCMovieViewController alloc] init];
+//    MLFile *file = [_allMedia objectAtIndex:index];
+//	movieViewController.file = file;
+//	[self.navigationController pushViewController:movieViewController animated:YES];
+//	[movieViewController release];
+}
+
+- (void) gridView:(AQGridView *)gridView didDeselectItemAtIndex:(NSUInteger)index {
+	MVLCMovieGridViewCell * cell = [gridView cellForItemAtIndex:index];
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+	[UIView setAnimationBeginsFromCurrentState:YES];
+	[UIView setAnimationDuration:MVLC_MOVIE_LIST_ANIMATION_DURATION];
+#if MVLC_MOVIE_LIST_ANIMATE_CELL
+	cell.overlayImageView.alpha = 1.0f;
+	cell.titleLabel.alpha = 1.0f;
+	cell.subtitleLabel.alpha = 1.0f;
+	cell.transform = CGAffineTransformIdentity;
+	self.view.alpha = 1.0f;
+#else
+	cell.contentView.backgroundColor = [UIColor clearColor];
+	cell.overlayImageView.alpha = 1.0f;
+	cell.posterImageView.alpha = 1.0f;
+	self.view.transform = CGAffineTransformIdentity;
+#endif
+	//	cell.frame = self.view.bounds;
+	[UIView commitAnimations];
 }
 @end
 
