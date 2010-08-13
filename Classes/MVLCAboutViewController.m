6151a26e7139f062eda3008b0f8a3e8364b420d9
Romain Goyet <romain.goyet@likid.org>
2010-08-13 18:03:48 +0200

    Beautiful "About" screen

--- Classes/MVLCAboutViewController.m
+++ Classes/MVLCAboutViewController.m
@@ -8,12 +8,35 @@
 
 #import "MVLCAboutViewController.h"
 
+#define MVLC_ABOUT_BEAUTIFY_WEBVIEW 1
+
+#if MVLC_ABOUT_BEAUTIFY_WEBVIEW
+void MVLCAboutViewControllerHideChildUIImageView(UIView * view) {
+	if ([view isKindOfClass:[UIImageView class]]) {
+		view.hidden = YES; // This removes the ugly shadow
+	} else {
+		view.backgroundColor = [UIColor clearColor]; // Make the background transparent
+		view.opaque = NO;
+		for (UIView * subview in [view subviews]) {
+			MVLCAboutViewControllerHideChildUIImageView(subview);
+		}
+	}
+}
+#endif
+
 @implementation MVLCAboutViewController
 @synthesize webView = _webView;
 - (void)viewDidLoad {
+	NSError * error = nil;
 	self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"MVLCBackgroundPattern.png"]];
 	self.title = @"About";
-	[self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.videolan.org"]]];
+#if MVLC_ABOUT_BEAUTIFY_WEBVIEW
+	MVLCAboutViewControllerHideChildUIImageView(self.webView);
+#endif	
+	[self.webView loadHTMLString:[NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"MVLCAboutContent" ofType:@"html"]
+														   encoding:NSUTF8StringEncoding
+															  error:&error]
+						 baseURL:nil];
 }
 
 - (void)dealloc {
