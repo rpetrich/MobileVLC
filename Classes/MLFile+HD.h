1d10fdb0e5088038522f4271edec3ea8c742d2ae
Romain Goyet <romain.goyet@likid.org>
2010-09-01 12:49:31 +0200

    Adding a (stub) category on MLFile to determine wether the file is HD or not

---
+++ Classes/MLFile+HD.h
@@ -0,0 +1,14 @@
+//
+//  MLFile+HD.h
+//  MobileVLC
+//
+//  Created by Romain Goyet on 01/09/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MediaLibraryKit/MLFile.h>
+
+@interface MLFile (HD)
+@property (readonly) BOOL isHD;
+@end
---
+++ Classes/MLFile+HD.m
@@ -0,0 +1,15 @@
+//
+//  MLFile+HD.m
+//  MobileVLC
+//
+//  Created by Romain Goyet on 01/09/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "MLFile+HD.h"
+
+@implementation MLFile (HD)
+- (BOOL)isHD {
+	return YES;
+}
+@end
