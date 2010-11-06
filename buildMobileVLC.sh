85dc7d5a8634e5c17d674f2c0e0a1dd2140ebfd6
Pierre d'Herbemont <pdherbemont@free.fr>
2010-11-06 14:38:04 +0100

    buildMobileVLC.sh: Add a way to build for the simulator.

--- buildMobileVLC.sh
+++ buildMobileVLC.sh
@@ -1,25 +1,70 @@
 #!/bin/bash
 
-git clone ssh://git@dev.applidium.com/var/git/vlc.git
-git clone ssh://git@dev.applidium.com/var/git/MediaLibraryKit.git
-git clone ssh://git@dev.applidium.com/var/git/MobileVLC.git
+set -e
 
-pushd vlc
-pushd extras/package/ios
-./build_for_iOS.sh
-popd
-pushd projects/macosx/framework
+usage()
+{
+cat << EOF
+usage: $0 [-s]
+
+OPTIONS
+   -s       Build for simulator
+EOF
+}
+
+spushd()
+{
+     pushd "$1" 2>&1> /dev/null
+}
+
+spopd()
+{
+     popd 2>&1> /dev/null
+}
+
+while getopts "ht:d:b:i:" OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         s)
+             TARGET=Simulator
+             ;;
+         ?)
+             usage
+             exit 1
+             ;;
+     esac
+done
+shift $(($OPTIND - 1))
+
+if [ "x$1" != "x" ]; then
+    usage
+    exit 1
+fi
+   
+git clone git://git.videolan.org/vlc.git
+git clone git://github.com/pdherbemont/MediaLibraryKit.git
+git clone git://git.videolan.org/MobileVLC.git
+
+spushd vlc
+spushd extras/package/ios
+./build_for_iOS.sh $(TARGET)
+spopd
+spushd projects/macosx/framework
 xcodebuild -project MobileVLCKit.xcodeproj -target "Aggregate static plugins" -configuration "Release"
 xcodebuild -project MobileVLCKit.xcodeproj -target "MobileVLCKit" -configuration "Release"
-popd
-popd
+spopd
+spopd
 
-pushd MediaLibraryKit
+spushd MediaLibraryKit
 ln -s ../../vlc/projects/macosx/framework/build/Release-iphoneos External/MobileVLCKit
 xcodebuild -project MobileMediaLibraryKit.xcodeproj -configuration "Release"
-popd
+spopd
 
-pushd MobileVLC
+spushd MobileVLC
 ln -s ../../vlc/projects/macosx/framework/build/Release-iphoneos External/MobileVLCKit
 ln -s ../../MediaLibraryKit/build/Release-iphoneos External/MediaLibraryKit
-popd
+spopd
