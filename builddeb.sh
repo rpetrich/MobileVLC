42767e3f41b80b60373981f964543e9a00367519
Ryan Petrich <rpetrich@gmail.com>
2011-01-08 10:44:00 -0700

    Replace com.zodttd.vlc4iphone; Make ZodTTD the package maintainer; Add proper depends on 3.2+ and ARMv7

--- builddeb.sh
+++ builddeb.sh
@@ -4,5 +4,5 @@ mkdir -p stage/Applications
 cp -r build/Release-iphoneos/VLC.app stage/Applications
 mkdir -p stage/DEBIAN
 cp control prerm stage/DEBIAN
-rm com.rpetrich.vlc_1.1.0_iphoneos.deb 2> /dev/null || true
-dpkg-deb -Zbzip2 -b stage com.rpetrich.vlc_1.1.0_iphoneos.deb
+rm com.zodttd.vlc4iphone_1:1.1.1_iphoneos.deb 2> /dev/null || true
+dpkg-deb -Zbzip2 -b stage com.zodttd.vlc4iphone_1:1.1.1_iphoneos.deb
